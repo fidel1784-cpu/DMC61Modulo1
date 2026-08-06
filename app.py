c3d7952c3253468feb4b2ab32c07be6a914eea2f
fidel1784-cpu <fidel1784@gmail.com>
2026-08-05 21:59:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
-st.write("Elaborado por: Carlos Carrillo")
+st.write("Elaborado por: Fidel Bringas")
 
 valor_inicial = st.number_input("Ingrese el valor inicial")
 valor_final = st.number_input("Ingrese el valor final")
