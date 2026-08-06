0f5f50606a76c8a2ea6167b9926901b6806c1622
fidel1784-cpu <fidel1784@gmail.com>
2026-08-05 22:04:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,53 @@
 import streamlit as st
 
+ 
+
 st.title("Especialización Python for Analytics")
+
 st.sidebar.title("Parámetros")
-st.write("Elaborado por: Fidel Bringas")
 
-valor_inicial = st.number_input("Ingrese el valor inicial")
-valor_final = st.number_input("Ingrese el valor final")
+st.write("Elaborado por: Carlos Carrillo")
 
-lista_numeros = list(range(int(valor_inicial), int(valor_final)))
+ 
 
-st.write(lista_numeros)
+ 
 
+ 
 
-modulos = st.sidebar.selectbox ("Seleccione un módulo", ["Módulo Listas","Módulo Funciones"])
+modulos = st.sidebar.selectbox ("Selecione un módulo", ["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
 
-if modulos == "Módulos Listas":
+ 
+
+if modulos == "Módulo Listas":
+
+  
+
   st.write("Bienvenido al módulo Listas")
+
+  
+
   valor_inicial = st.number_input("Ingrese el valor inicial")
+
   valor_final = st.number_input("Ingrese el valor final")
 
-lista_numeros = list(range(int(valor_inicial), int(valor_final)))
+  
 
-st.write(lista_numeros)
+  lista_numeros = list(range(int(valor_inicial), int(valor_final)))
+
+  st.write(lista_numeros)
+
+  
 
 elif modulos == "Módulo Arreglos":
 
-  st.write("Bienvenido al módulo Arreglos")
+  
+
+  st.write("Bienvenido al módulo de Arreglos")
+
+  
 
 else:
+
   
-  st.write("Bienvenido al módulo Funciones")
+
+  st.write("Bienvenido al módulo de Funciones")
